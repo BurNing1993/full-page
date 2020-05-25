5506646bd45828bf0c1f79e524bf71f626af0fd5
BurNing1993 <786678257@qq.com>
2020-05-25 11:06:47 +0800

    init

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'production',
+  entry: './src/index.ts',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js']
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'full-page.js',
+    library: 'full-page',
+    libraryTarget: 'umd'
+  },
+  devtool: 'inline-source-map',
+};
